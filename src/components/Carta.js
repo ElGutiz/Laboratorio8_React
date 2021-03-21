f2e4ba182176e6b37259c12982b5e34e82a22a37
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-03-21 12:10:25 -0600

    Carta Component Creation

---
+++ src/components/Carta.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../Carta.css';
+
+class Carta extends React.Component {
+    render() {
+        return (
+            <div className="carta">
+
+            </div>
+        );
+    }
+};
+
+export default Carta;
