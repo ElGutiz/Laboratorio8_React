eaff878e7e6c807cd9c2b5a3a38ee8088e5bc636
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-03-21 14:41:48 -0600

    Font-awesome implementation

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import Header from './components/Header';
 import Tablero from './components/Tablero';
 
 import './index.css';
-import 'font-awesome/css/font-awesome.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
 ReactDOM.render(<Header />, document.getElementById('root'));
 ReactDOM.render(<Tablero/>, document.getElementById('root2'));
