523a20daf58114e2b5f0437b6edfbc07a06490d5
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-03-22 01:13:54 -0600

    Fixed Card onClick and style

--- src/components/Carta.js
+++ src/components/Carta.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import FlipCard from 'react-card-flip';
+import FrontCard from './Front_card';
+import BackCard from "./Back_card";
 
 import '../Carta.css';
 
 class Carta extends React.Component {
+    constructor() {
+        super();
+          this.state = {
+          isFlipped: false
+        };
+        this.handleClick = this.handleClick.bind(this);
+      }
+    
+      handleClick(e) {
+        e.preventDefault();
+        this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+      }
+
     render() {
         return (
             <div className="carta">
-
+                <FlipCard isFlipped={this.state.isFlipped} flipDirection="vertical">
+                    <FrontCard handleClick={this.handleClick}>
+                        
+                    </FrontCard>
+                    <BackCard handleClick={this.handleClick}>
+                        
+                    </BackCard>
+                </FlipCard>
             </div>
         );
     }
