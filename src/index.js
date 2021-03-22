f28af0dcf4719a6a5985acb86924e27d450d147e
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-03-22 01:14:11 -0600

    Index Fix

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import Header from './components/Header';
-// import Tablero from './components/Tablero';
-
-// import contruirBaraja from './utils/Baraja';
-
 import './index.css';
-// import '@fortawesome/fontawesome-free/js/fontawesome';
-// import '@fortawesome/fontawesome-free/js/solid';
-// import '@fortawesome/fontawesome-free/js/regular';
-// import '@fortawesome/fontawesome-free/js/brands';
-
 import StartApp from './simpleComponent/startApp';
-
-
-// ReactDOM.render(<Header />, document.getElementById('root'));
-// ReactDOM.render(<Tablero/>, document.getElementById('root2'));
 
 ReactDOM.render(
     <StartApp />, 
