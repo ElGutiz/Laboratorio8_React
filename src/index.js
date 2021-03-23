33ea7e05b49b64a9ee5056fade7e3791829b13d2
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2021-03-22 19:16:37 -0600

    Installation Font-awesome
    Fixed icons inside cards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'font-awesome/css/font-awesome.css';
+
 import './index.css';
 import StartApp from './simpleComponent/startApp';
 
